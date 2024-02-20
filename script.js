e1352174fc1d066b5bebd0560f1810e7833e7fcc
Aakash Maroju <92659675+AAKASHMAROJU@users.noreply.github.com>
2024-02-20 12:36:06 +0530

    Update script.js - optimized the code and also added localStorage to keep track of the last used mode by user

--- script.js
+++ script.js
@@ -2,19 +2,28 @@ const toggleButton = document.getElementById("inp-check");
 
 const l_d_mode = document.getElementById("light-dark-mode");
 
+const contact_img = document.querySelector("#img-contact");
+const search_img = document.querySelector("#img-about");
+const settings_img = document.querySelector("#img-settings");
+
+const imgMode = (color) => {
+  contact_img.src = `images/contact_${color}.svg`;
+  search_img.src = `images/search_${color}.svg`;
+  settings_img.src = `images/settings_${color}.svg`;
+};
+
 const darkMode = () => {
-  l_d_mode.children[1].classList.remove("fa-sun");
-  l_d_mode.children[1].classList.add("fa-moon");
+  l_d_mode.children[1].classList.replace("fa-sun", "fa-moon");
   l_d_mode.style.color = "white";
   l_d_mode.children[0].textContent = "Dark";
+  imgMode("dark");
 };
 
 const lightMode = () => {
-  l_d_mode.children[1].classList.remove("fa-moon");
-  l_d_mode.children[1].classList.add("fa-sun");
-  l_d_mode.children[0].style.color = "white";
+  l_d_mode.children[1].classList.replace("fa-moon", "fa-sun");
   l_d_mode.children[1].style.color = "white";
   l_d_mode.children[0].textContent = "Light";
+  imgMode("light");
 };
 
 const changeTheme = (e) => {
@@ -24,11 +33,25 @@ const changeTheme = (e) => {
     // dark Mode
     document.documentElement.setAttribute("data-theme", "dark");
     darkMode();
+    localStorage.setItem("theme", "dark");
   } else {
     // Light Mode
     document.documentElement.setAttribute("data-theme", "white");
     lightMode();
+    localStorage.setItem("theme", "light");
   }
 };
 
 toggleButton.addEventListener("change", changeTheme);
+
+const currTheme = localStorage.getItem("theme");
+if (currTheme) {
+  document.documentElement.setAttribute("data-theme", currTheme);
+  if (currTheme === "dark") {
+    darkMode();
+    toggleButton.checked = true;
+  } else {
+    lightMode();
+    toggleButton.checked = false;
+  }
+}
